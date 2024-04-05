21fff4cd7aff1045d79b61e647abe89a51b13c8e
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-04-05 20:16:32 +0800

    Button click logging works

--- website/app.js
+++ website/app.js
@@ -12,6 +12,7 @@ const PORT = 8080;
 app.set('view engine', 'ejs');
 
 app.use(cookieParser());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
