42d4ac9d2b544f718a2778b454f60f5c030e06bf
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-04-05 12:46:36 +0800

    Added products

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
@@ -10,6 +11,13 @@ app.set('view engine', 'ejs');
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const products = [
+    {id: '1', name: 'Product 1', description: 'Some text here', price: '5 SGD' },
+    {id: '2', name: 'Product 2', description: 'One more text here', price: '15 SGD' },
+    {id: '3', name: 'Product 3', description: 'Another text here', price: '20 SGD' },
+]
 
 app.get('/', (req, res) => {
     res.cookie('username', uuidv4(), { maxAge: 900000, httpOnly: true });
@@ -17,19 +25,36 @@ app.get('/', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-    res.render('pages/products');
+    res.render('pages/products', {
+        products: products,
+    });
 });
 
-app.get('/confirm', (req, res) => {
-    res.render('pages/confirm');
+app.post('/confirm', (req, res) => {
+    const product = products.find(
+        product => product.id === req.body.id
+    )
+    res.render('pages/confirm', { 
+        product: product,
+    });
 });
 
-app.get('/payment', (req, res) => {
-    res.render('pages/payment');
+app.get('/payment/:id', (req, res) => {
+    const product = products.find(
+        product => product.id === req.params.id
+    )
+    res.render('pages/payment', { 
+        product: product,
+    });
 });
 
-app.get('/receipt', (req, res) => {
-    res.render('pages/receipt');
+app.get('/receipt/:id', (req, res) => {
+    const product = products.find(
+        product => product.id === req.params.id
+    )
+    res.render('pages/receipt', { 
+        product: product,
+    });
 });
 
 app.listen(PORT, () => {
